c2c5b15e6d47096fba535b598ceeebc1da678bb6
Isulew <netcookies@gmail.com>
2019-01-04 16:38:30 +0800

    rsp for product use only

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -9,6 +9,6 @@ if [ ! -d "/u01/app/oracle/product/11.2.0/dbhome_1" ]; then
 fi
 
 rm -f /etc/localtime \
-&& ln -s /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+&& ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 su oracle -c "/assets/entrypoint_oracle.sh"
